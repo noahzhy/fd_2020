ac0a4da775ecd76a71247222111b1c0bfae940b3
noahzhy <noahzhy@gmail.com>
2020-05-21 15:59:09 +0900

    ds

---
+++ fd_testing_tools/IR_test/data_collect.py
@@ -0,0 +1,13 @@
+import os
+import datetime
+import numpy as np
+import pandas as pd
+
+
+def data_collect(df=pd.DataFrame()):
+    df = pd.DataFrame(df, index=False)
+
+
+
+if __name__ == "__main__":
+    pass

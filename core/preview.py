4fd4a27166d2d1a27550c38b8b9c16c8f539a52b
noahzhy <noahzhy@gmail.com>
2020-07-08 22:04:46 +0900

    add

--- core/preview.py
+++ core/preview.py
@@ -14,6 +14,7 @@ import numpy as np
 from tkinter.filedialog import askopenfilename
 import os
 import glob
+import threading as th
 
 
 class App:
@@ -45,16 +46,17 @@ class App:
         bar.pack()
 
         operation_frame = LabelFrame(self.app, text="Operation")
-        operation_frame.pack(side='top', fill='both', expand=True, padx=10, pady=5)
+        operation_frame.pack(side='top', fill='both',
+                             expand=True, padx=10, pady=5)
 
-        open_btn = Button(operation_frame, text="Open [Ctrl+O]", command=self.select_path)
+        open_btn = Button(
+            operation_frame, text="Open [Ctrl+O]", command=self.select_path)
         open_btn.pack(side='left', fill='x', expand=True, padx=10, pady=10)
 
-        play_btn = Button(operation_frame, text="Play [Space]", command=self.start_play)
+        play_btn = Button(operation_frame, width=10,
+                          text="Play [Space]", command=self.start_pause)
+        play_btn.bind("<space>", self.start_pause)
         play_btn.pack(side='left', fill='x', expand=True, padx=10, pady=10)
-
-        stop_btn = Button(operation_frame, text="Stop [Space]", command=self.stop_play)
-        stop_btn.pack(side='left', fill='x', expand=True, padx=10, pady=10)
 
         start_btn = Button(operation_frame, text="Start [<]")
         start_btn.pack(side='left', fill='x', expand=True, padx=10, pady=10)
@@ -69,12 +71,18 @@ class App:
         label_frame.pack(side='top', fill='both', expand=True, padx=10, pady=5)
 
         v = IntVar()
-        Radiobutton(label_frame, text='One', variable=v, value=1).pack(side='left')
-        Radiobutton(label_frame, text='Two', variable=v, value=2).pack(side='left')
-        Radiobutton(label_frame, text='Three', variable=v, value=3).pack(side='left')
-        Radiobutton(label_frame, text='One', variable=v, value=4).pack(side='left')
-        Radiobutton(label_frame, text='Two', variable=v, value=5).pack(side='left')
-        Radiobutton(label_frame, text='Three', variable=v, value=6).pack(side='left')
+        Radiobutton(label_frame, text='One', variable=v,
+                    value=1).pack(side='left')
+        Radiobutton(label_frame, text='Two', variable=v,
+                    value=2).pack(side='left')
+        Radiobutton(label_frame, text='Three',
+                    variable=v, value=3).pack(side='left')
+        Radiobutton(label_frame, text='One', variable=v,
+                    value=4).pack(side='left')
+        Radiobutton(label_frame, text='Two', variable=v,
+                    value=5).pack(side='left')
+        Radiobutton(label_frame, text='Three',
+                    variable=v, value=6).pack(side='left')
 
         result_list = Listbox(self.app, width=100)
         result_list.pack(padx=10, pady=10)
@@ -82,6 +90,7 @@ class App:
         self.left_view = left_view
         self.right_view = right_view
         self.bar = bar
+        self.play_btn = play_btn
 
         self.app.mainloop()
 
@@ -97,24 +106,32 @@ class App:
         _, img = self.cap.read()
         return self.openCV_to_PhotoImage(img)
 
-    def update_win(self, pos=0, fix=100):
-        frame_num = int(len(self.data)*self.bar.get()/(self.bar['to']+1))
-        # frame_num = int(self.bar.get()/25*8)
+    def update_win(self, pos=0):
+        frame_num = int(
+            pos * (self.cap.get(cv2.CAP_PROP_FRAME_COUNT)*0.95) / len(self.data)
+        ) - 25
 
-        if frame_num >= len(self.data):
-            frame_num = len(self.data)-1
-
-        img_left = self.get_fir_frame(frame_num)
+        img_left = self.get_fir_frame(self.bar.get())
         self.left_view.config(image=img_left)
         self.left_view.image = img_left
 
-        img_right = self.get_video_frame(self.bar.get()-fix)
+        img_right = self.get_video_frame(frame_num)
         self.right_view.config(image=img_right)
         self.right_view.image = img_right
         self.app.update()
 
+    def start_pause(self, event=None):
+        # self.play_status = True
+        if not self.play_status:
+            start_th = th.Thread(target=self.start_play)
+            start_th.start()
+            self.play_btn['text'] = 'Pause [Space]'
+            self.play_status = True
+        else:
+            self.play_btn['text'] = 'Play [Space]'
+            self.play_status = False
+
     def start_play(self):
-        self.play_status = True
         current_pos = self.bar.get()
         while current_pos <= self.bar['to']:
             if self.play_status:
@@ -125,25 +142,14 @@ class App:
             else:
                 break
 
-    def stop_play(self):
-        self.play_status = False
-
     @staticmethod
     def raw_img(frame):
         return resize(frame, (320, 240), interpolation=INTER_NEAREST)
 
     @staticmethod
     def data_to_frame(data):
-        def normalization(data):
-            np.array(data)
-            _range = data.max() - data.min()
-            return (data - data.min()) / _range
-
-
         out_data = None
         out_data = cv2.normalize(data, out_data, 0, 255, cv2.NORM_MINMAX)
-        # print(out_data)
-        # print(out_data)
         img_gray = (out_data).astype('uint8')
         heatmap_g = img_gray.astype('uint8')
         frame = applyColorMap(heatmap_g, COLORMAP_JET)
@@ -159,29 +165,19 @@ class App:
         path_ = askopenfilename()
         father_path = os.path.dirname(os.path.dirname(path_))
         date = os.path.basename(path_).split('.')[0].split('_')[0:2]
-        data_path = glob.glob('{}/data/{}_{}_*.csv'.format(father_path, date[0], date[1]))[0]
-        video_path = glob.glob('{}/video/{}_{}_*.mp4'.format(father_path, date[0], date[1]))[0]
+        data_path = glob.glob(
+            '{}/data/{}_{}_*.csv'.format(father_path, date[0], date[1]))[0]
+        video_path = glob.glob(
+            '{}/video/{}_{}_*.mp4'.format(father_path, date[0], date[1]))[0]
         # print(data_path, video_path)
         self.data = pd.read_csv(data_path, index_col=None).iloc[:, 2:]
         self.cap = cv2.VideoCapture(video_path)
-        self.bar['to'] = self.cap.get(cv2.CAP_PROP_FRAME_COUNT)
+        self.bar['to'] = len(self.data)
         self.bar['tickinterval'] = int(self.bar['to']/10)
-        print(len(self.data), self.bar['to'])
         self.update_win(0)
 
-        # while True:
-        #     try:
-        #         self.update_win()
-        #     except Exception as e:
-        #         break
-
-        # self.auto_play()
+        # self.start_pause()
 
 
 if __name__ == "__main__":
-    # data_path = r'data\20200628_183032_mlx90640_01_light_natural.csv'
-    # video_path = r'video\20200628_183032_mlx90640_01_light_natural.mp4'
-
     app = App()
-    # data = app.load_data(data_path, video_path)
-    # app.update_win(78)

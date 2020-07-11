4edef7e9e7b2f6cd0cfbcaf9f9943a80f8210972
noahzhy <noahzhy@gmail.com>
2020-07-11 23:02:46 +0900

    add test

--- core/test.py
+++ core/test.py
@@ -1,22 +1,52 @@
+# univariate cnn lstm example
+from numpy import array
 from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.layers import Embedding
-from keras.layers import Conv1D, GlobalAveragePooling1D, MaxPooling1D
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers import TimeDistributed
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
 
-seq_length = 64
+# split a univariate sequence into samples
+def split_sequence(sequence, n_steps):
+	X, y = list(), list()
+	for i in range(len(sequence)):
+		# find the end of this pattern
+		end_ix = i + n_steps
+		# check if we are beyond the sequence
+		if end_ix > len(sequence)-1:
+			break
+		# gather input and output parts of the pattern
+		seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+		X.append(seq_x)
+		y.append(seq_y)
+	return array(X), array(y)
 
+# define input sequence
+raw_seq = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+# choose a number of time steps
+n_steps = 4
+# split into samples
+X, y = split_sequence(raw_seq, n_steps)
+# reshape from [samples, timesteps] into [samples, subsequences, timesteps, features]
+n_features = 1
+n_seq = 2
+n_steps = 2
+X = X.reshape((X.shape[0], n_seq, n_steps, n_features))
+# define model
 model = Sequential()
-model.add(Conv1D(64, 3, activation='relu', input_shape=(seq_length, 100)))
-model.add(Conv1D(64, 3, activation='relu'))
-model.add(MaxPooling1D(3))
-model.add(Conv1D(128, 3, activation='relu'))
-model.add(Conv1D(128, 3, activation='relu'))
-model.add(GlobalAveragePooling1D())
-model.add(Dropout(0.5))
-model.add(Dense(1, activation='sigmoid'))
-
-model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
-              metrics=['accuracy'])
-
-model.summary()
+model.add(TimeDistributed(Conv1D(filters=64, kernel_size=1, activation='relu'), input_shape=(None, n_steps, n_features)))
+model.add(TimeDistributed(MaxPooling1D(pool_size=2)))
+model.add(TimeDistributed(Flatten()))
+model.add(LSTM(50, activation='relu'))
+model.add(Dense(1))
+model.compile(optimizer='adam', loss='mse')
+# fit model
+model.fit(X, y, epochs=500, verbose=0)
+# demonstrate prediction
+x_input = array([60, 70, 80, 90])
+x_input = x_input.reshape((1, n_seq, n_steps, n_features))
+print(x_input)
+yhat = model.predict(x_input, verbose=0)
+print(yhat)
--- core/train.py
+++ core/train.py
@@ -1,8 +1,4 @@
-import glob
-import collections
-import re
-import random
-import os
+import glob, collections, re, random, os
 import argparse
 import numpy as np
 import pandas as pd
